26fc891ab2a733140bc8bd39bafb8099230d8198
Zhen Wu <61070056+Zhw0831@users.noreply.github.com>
2022-10-11 00:58:29 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,15 @@
+document.write(`
+<header>
+  <div class="name">Zhen Wu</div>
+  <nav>
+    <ul>
+      <li><a href="index.html"> Home </a></li>
+      <li><a href="research.html"> Research </a></li>
+      <li><a href="project.html"> Projects </a></li>
+      <li><a href="award.html"> Awards </a></li>
+      <li><a href="occ.html"> Extracurricular </a></li>
+      <li><a href="course.html"> Courses </a></li>
+    </ul>
+  </nav>
+</header>
+`);
